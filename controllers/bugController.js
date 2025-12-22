e8de87039d00c956a7c37112ecbfbde8b69e79f0
John Samuel P <jsamuelp181@gmail.com>
2025-12-22 06:57:27 +0530

    update by id

--- controllers/bugController.js
+++ controllers/bugController.js
@@ -1,3 +1,80 @@
-exports.createBug = (req,res)=>{
-    res.status(201).json({message:"Bug created successfully"});
+const Bug = require("../models/Bug");
+exports.getBugs = async (req, res) => {
+  try {
+    const bugs = await Bug.find().sort({ createdAt: -1 }); 
+    return res.status(200).json({ bugs });
+  } catch (error) {
+    return res.status(500).json({
+      message: "server error",
+      error,
+    });
+  }
+};
+exports.getBugsbyId = async (req, res) => {
+  const bugId = req.params.id;
+  try {
+    const bug = await Bug.findById(bugId);
+    if (!bug) {
+      return res.status(404).json({ message: "bug not found" });
+    }   
+    return res.status(200).json({ bug });
+  } catch (error) {
+    return res.status(500).json({
+      message: "server error",
+      error,
+    });
+  }
+};
+exports.updateBug=async(req,res)=>{
+    const bugId=req.params.id;
+    const {status,priority}=req.body;
+    try{
+        const bug=await Bug.findById(bugId);    
+        if(!bug){
+            return res.status(404).json({message:"bug not found"});
+        }
+        if(status){
+            bug.status=status;
+        }   
+        if(priority){
+            bug.priority=priority;
+        }
+        await bug.save();
+        res.status(200).json({
+            message:"bug updated successfully",
+            bug,
+        })
+    }catch(error){
+        res.status(500).json({
+            message:"server error",
+            error,
+        })
+    }
+}
+exports.createBug = async(req,res)=>{
+    const {title,description,priority}=req.body;
+    const userId=req.user.id;
+    if(!title||!description){
+        return res.status(400).json({
+            message:"please provide all required fields"
+        })
+    }
+    try{
+        const newBug =await Bug.create({
+            title,
+            description,
+            priority: priority ||"Low",
+            createdBy:userId,
+        })
+        res.status(201).json({
+            message:"bug created successfully",
+            bug:newBug,
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            message:"server error",
+            error,
+        })
+    }
 }
--- routes/bugRoutes.js
+++ routes/bugRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
 const authMiddleware = require('../middleware/authMiddleware.js');
-const {createBug} = require('../controllers/bugController.js');
+const {createBug,getBugs,getBugsbyId,updateBug} = require('../controllers/bugController.js');
 router.post('/', authMiddleware, createBug);
+router.get('/', authMiddleware, getBugs);
+router.get('/:id', authMiddleware, getBugsbyId);
+router.patch('/:id', authMiddleware, updateBug);
 module.exports = router;
